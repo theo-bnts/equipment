57c986ccaf2f615ae64c68bf5fddf1aadfe8c32f
Théo Bontemps <theo2bont@gmail.com>
2024-05-25 16:37:46 +0200

    /account/security/token endpoint

--- API/entities/Token.js
+++ API/entities/Token.js
@@ -1,4 +1,5 @@
 import DatabasePool from './tools/DatabasePool.js';
+import User from './User.js';
 
 class Token {
   Id;
@@ -10,6 +11,7 @@ class Token {
   User;
 
   constructor(id, value, expiration, user) {
+    this.Id = id;
     this.Value = value;
     this.Expiration = expiration;
     this.User = user;
@@ -34,6 +36,7 @@ class Token {
         { _id: this.Id },
         {
           $set: {
+            value: this.Value,
             expiration: this.Expiration,
             id_user: this.User.Id,
           },
@@ -42,9 +45,26 @@ class Token {
   }
 
   static async isValidValue(value) {
+    const token = await DatabasePool
+      .getConnection()
+      .collection('token')
+      .findOne({ value });
+
+    return token !== null;
   }
 
   static async fromValue(value) {
+    const token = await DatabasePool
+      .getConnection()
+      .collection('token')
+      .findOne({ value });
+    
+    return new Token(
+      token._id,
+      token.value,
+      token.expiration,
+      await User.fromId(token.id_user),
+    );
   }
 }
 
