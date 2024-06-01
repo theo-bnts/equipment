8171c9f2da77cf302330bf7af8973b5621d01f98
Théo Bontemps <theo2bont@gmail.com>
2024-06-01 16:50:59 +0200

    authentification

--- site/src/app/services/auth.service.ts
+++ site/src/app/services/auth.service.ts
@@ -1,21 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
+import { environment } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'https://example.com';
-  
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<boolean> {
-    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, { email, password })
+    return this.http.post<{ datas: { value: string, expiration: string, user: any } }>(`${environment.API_BASE_URL}/account/token`, { user: { email_address: email, password: password } })
       .pipe(
         map(response => {
-          localStorage.setItem('token', response.token);
+          const token = response.datas.value;
+          const expiration = response.datas.expiration;
+          localStorage.setItem('token', token);
+          localStorage.setItem('tokenExpiration', expiration);
           return true;
         }),
         catchError(error => {
@@ -26,10 +28,44 @@ export class AuthService {
   }
 
   logout(): void {
-    localStorage.removeItem('token');
+    const token = localStorage.getItem('token');
+    if (token) {
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+      this.http.delete(`${environment.API_BASE_URL}/account/token`, { headers }).subscribe(
+        () => {
+          localStorage.removeItem('token');
+          localStorage.removeItem('tokenExpiration');
+        },
+        error => console.error(error)
+      );
+    }
   }
 
   isLoggedIn(): boolean {
-    return !!localStorage.getItem('token');
+    const token = localStorage.getItem('token');
+    const expiration = localStorage.getItem('tokenExpiration');
+    if (!token || !expiration) {
+      return false;
+    }
+    const now = new Date();
+    const expirationDate = new Date(expiration);
+    return now < expirationDate;
+  }
+
+  getUserInfo(): Observable<any> {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+      return this.http.get<{ datas: any }>(`${environment.API_BASE_URL}/account/user`, { headers })
+        .pipe(
+          map(response => response.datas),
+          catchError(error => {
+            console.error(error);
+            return of(null);
+          })
+        );
+    } else {
+      return of(null);
+    }
   }
 }
