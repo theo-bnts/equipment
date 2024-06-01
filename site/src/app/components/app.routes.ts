f44507e895d5481729cac2b8db76c69c33c5c1b2
DorianDescamps <93372714+DorianDescamps@users.noreply.github.com>
2024-06-01 18:04:48 +0200

    logout function

--- site/src/app/components/app.routes.ts
+++ site/src/app/components/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { AccountComponent } from './pages/account/account.component';
+import { AuthGuard } from '../services/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'account', component: AccountComponent, canActivate: [AuthGuard] }
 ];
---
+++ site/src/app/services/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
+  }
+}
