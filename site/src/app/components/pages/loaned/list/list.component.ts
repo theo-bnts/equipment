bbd4b03ebf99e78eb8bf4ad49003e5e8549823fe
Théo Bontemps <theo2bont@gmail.com>
2024-06-06 21:49:39 +0200

    rename loaned list

--- site/src/app/components/pages/loaned/list/list.component.ts
+++ site/src/app/components/pages/loaned/list/list.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
-import { ListComponent } from '../../../partials/list/list.component';
+import { ListComponent } from '../../../partials/loaned-list/loaned-list.component';
 
 @Component({
   selector: 'app-loaned-list',
   standalone: true,
   imports: [ListComponent],
-  templateUrl: './loaned-list.component.html',
-  styleUrls: ['./loaned-list.component.css']
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
 })
 export class LoanedListComponent { }
