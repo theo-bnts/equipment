62dd95c4519a7b86d70ea46fc3dcd9560526bb45
Théo Bontemps <theo2bont@gmail.com>
2024-06-08 19:25:06 +0200

    loan request form

--- site/src/app/components/partials/loan-request-form/loan-request-form.component.ts
+++ site/src/app/components/partials/loan-request-form/loan-request-form.component.ts
@@ -9,12 +9,31 @@ import { of } from 'rxjs';
 import { AccountService } from '../../../services/account.service';
 
 @Component({
-  selector: 'app-login-form',
+  selector: 'app-loan-request-form',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './loan-request-form.component.html',
   styleUrls: ['../../../../styles/form.css']
 })
 export class LoanRequestFormComponent {
+  loanRequestForm: FormGroup;
+  userOrganizations: any[] = [];
+
+  constructor(private formBuilder: FormBuilder, private authService: AccountService, private router: Router) {
+    this.loanRequestForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  get formControls() { return this.loanRequestForm.controls; }
+
+  isOrganizationSelected = false;
   
+  onLoanTypeChange(loantype: string) {
+    this.isOrganizationSelected = false;
+  }
+
+  onSubmit() {
+  }
 }
