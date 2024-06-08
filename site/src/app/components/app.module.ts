6490c76a02c68ae5e7b443369d84112a9b5efc5d
DorianDescamps <93372714+DorianDescamps@users.noreply.github.com>
2024-06-08 21:04:43 +0200

    auto logout

--- site/src/app/components/app.module.ts
+++ site/src/app/components/app.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
 import { HeaderComponent } from './partials/header/header.component';
-import { AuthInterceptor } from '../services/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,9 +20,7 @@ import { AuthInterceptor } from '../services/auth.interceptor';
     RouterModule.forRoot(routes),
     HeaderComponent
   ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
