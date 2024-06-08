4c2ed66f04c44fc8b3284e0310e0f9b6bfb8c9da
Théo Bontemps <theo2bont@gmail.com>
2024-06-08 21:56:45 +0200

    disconnect button externalisation

--- site/src/app/components/pages/account/account.component.ts
+++ site/src/app/components/pages/account/account.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { catchError, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 import { AccountFormComponent } from '../../partials/account-form/account-form.component';
+import { AccountService } from '../../../services/account.service';
 
 @Component({
   selector: 'app-account',
   standalone: true,
   imports: [CommonModule, AccountFormComponent],
   templateUrl: './account.component.html',
-  styleUrls: ['../../../../styles/page.css']
+  styleUrls: ['../../../../styles/page.css', './account.component.css']
 })
-export class AccountPageComponent {}
+export class AccountPageComponent {
+  constructor(private accountService: AccountService, private router: Router) {}
+
+  logout() {
+    this.accountService
+      .logout()
+      .pipe(
+        tap(() => this.router.navigate(['/login'])),
+        catchError(() => {
+          alert('Logout failed');
+          return of();
+        })
+      )
+      .subscribe();
+  }
+}
