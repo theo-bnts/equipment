ea95f416c77031a0ef68c004a27adf96cb3dc5c9
Théo Bontemps <theo2bont@gmail.com>
2024-06-10 15:14:33 +0200

    /administration/loaned/list page

---
+++ site/src/app/components/partials/administration-loaned-list/administration-loaned-list.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { tap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+
+import { UserService } from '../../../services/user.service';
+
+@Component({
+  selector: 'app-loaned-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './loaned-list.component.html',
+  styleUrls: ['../../../../styles/table.css']
+})
+export class AdministrationLoanedListComponent implements OnInit {
+  loans: any[] = [];
+
+  constructor(private router: Router, private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getLoans().subscribe(
+      data => {
+        this.loans = data;
+        this.sortLoans();
+      },
+      error => console.error('Failed to load loans', error)
+    );
+  }
+
+  sortLoans() {
+    this.loans.sort((a, b) => {
+      const dateA = new Date(a.loan_date);
+      const dateB = new Date(b.loan_date);
+      return dateA.getTime() - dateB.getTime();
+    });
+  }
+
+  isReturnDateExceeded(returnDate: string): boolean {
+    const currentDate = new Date();
+    const returnDateObj = new Date(returnDate);
+    return returnDateObj < currentDate;
+  }
+
+  onLoanReturnRequest(equipmentCode: string) {
+    this.userService.returnLoan(equipmentCode)
+      .pipe(
+        tap(() => location.reload()),
+        catchError(() => {
+          alert('Failed to ask for loan return');
+          return of();
+        })
+      )
+      .subscribe();
+  }
+}
