501cf3589e431dc6776e66d3ceb812475e9e3c83
Théo Bontemps <theo2bont@gmail.com>
2024-06-10 15:39:52 +0200

    wip

--- site/src/app/components/app.routes.ts
+++ site/src/app/components/app.routes.ts
@@ -1,14 +1,16 @@
 import { Routes } from '@angular/router';
 
 import { AccountPageComponent } from './pages/account/account.component';
-import { UserAuthGuard } from '../services/user.auth.guard';
-import { AdministratorAuthGuard } from '../services/administrator.auth.guard';
-import { UserHomePageComponent } from './pages/user/home/home.component';
+
 import { AdministrationHomePageComponent } from './pages/administration/home/home.component';
+import { AdministrationLoanedListPageComponent } from './pages/administration/loaned/list/list.component';
+import { AdministratorAuthGuard } from '../services/administrator.auth.guard';
 import { LoanableListPageComponent } from './pages/loanable/list/list.component';
 import { LoanedListPageComponent } from './pages/loaned/list/list.component';
 import { LoanRequestPageComponent } from './pages/loan/request/request.component';
 import { LoginPageComponent } from './pages/login/login.component';
+import { UserAuthGuard } from '../services/user.auth.guard';
+import { UserHomePageComponent } from './pages/user/home/home.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full' , redirectTo: '/account'},
@@ -19,5 +21,5 @@ export const routes: Routes = [
   { path: 'loan/request', pathMatch: 'full', component: LoanRequestPageComponent, canActivate: [UserAuthGuard] },
   { path: 'loaned/list', pathMatch: 'full', component: LoanedListPageComponent, canActivate: [UserAuthGuard] },
   { path: 'administration/home', pathMatch: 'full', component: AdministrationHomePageComponent, canActivate: [AdministratorAuthGuard] },
-  { path: 'administration/loaned/list', pathMatch: 'full', component: LoanedListPageComponent, canActivate: [AdministratorAuthGuard] },
+  { path: 'administration/loaned/list', pathMatch: 'full', component: AdministrationLoanedListPageComponent, canActivate: [AdministratorAuthGuard] }
 ];
